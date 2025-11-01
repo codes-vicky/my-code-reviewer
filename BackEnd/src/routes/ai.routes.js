1f943efa0b662137be6a343b099beac4cd0a020d
codes-vicky <gy944307@gmail.com>
2025-10-31 23:34:16 -0700

    colon remove

--- BackEnd/src/routes/ai.routes.js
+++ BackEnd/src/routes/ai.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const aiController = require("../controllers/ai.controller")
 
-const router = express.Router();
+const router = express.Router()
 
 
 router.post("/get-review", aiController.getReview)
